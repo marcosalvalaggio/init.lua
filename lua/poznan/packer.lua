d40c1997be2b512a0344c2000fbf988892ba3a75
mar.salvalaggio@gmail.com <55287795+Salva32@users.noreply.github.com>
2023-08-01 11:33:34 +0200

    wip

--- lua/poznan/packer.lua
+++ lua/poznan/packer.lua
@@ -1,6 +1,6 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
+-- This file can be loaded by calling lua require('plugins') from your init.vim
 
--- Only required if you have packer configured as `opt`
+-- Only required if you have packer configured as opt
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
@@ -8,64 +8,40 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   
   use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
+   'nvim-telescope/telescope.nvim', tag = '0.1.1',
+   -- or                            , branch = '0.1.x',
+   requires = { {'nvim-lua/plenary.nvim'} }
   }
 
-  -- use({
-	  -- 'rose-pine/neovim',
-	  -- as = 'rose-pine',
-	  -- config = function()
-		  -- vim.cmd('colorscheme rose-pine')
-	  -- end
-  -- })
-
-  use("Mofiqul/vscode.nvim")
-  use('nvim-treesitter/nvim-treesitter', {run =  ':TSUpdate'})
-  use('nvim-treesitter/playground')
+   use({
+    'rose-pine/neovim',
+    as = 'rose-pine',
+    config = function()
+     vim.cmd('colorscheme rose-pine')
+    end
+   })
+  
   use("theprimeagen/harpoon")
   use({
-	  "kdheepak/lazygit.nvim",
-	  -- optional for floating window border decoration
-	  requires = {
-		  "nvim-lua/plenary.nvim",
-	  },
+   "kdheepak/lazygit.nvim",
+   -- optional for floating window border decoration
+   requires = {
+    "nvim-lua/plenary.nvim",
+   },
   })
   use("folke/zen-mode.nvim")
   use("tpope/vim-commentary")
   use("vim-airline/vim-airline")
-  use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v2.x',
-	  requires = {
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},             -- Required
-		  {                                      -- Optional
-		  'williamboman/mason.nvim',
-		  run = function()
-			  pcall(vim.cmd, 'MasonUpdate')
-		  end,
-	  },
-	  {'williamboman/mason-lspconfig.nvim'}, -- Optional
-
-	  -- Autocompletion
-	  {'hrsh7th/nvim-cmp'},     -- Required
-	  {'hrsh7th/cmp-nvim-lsp'}, -- Required
-	  {'L3MON4D3/LuaSnip'},     -- Required
-  }
-}
-
-use("lukas-reineke/indent-blankline.nvim")
-use("airblade/vim-gitgutter")
-use("liuchengxu/vista.vim")
-
--- Markdown Preview
+  use("lukas-reineke/indent-blankline.nvim")
+  use("liuchengxu/vista.vim")
+  use("airblade/vim-gitgutter")
  use({
-   "iamcco/markdown-preview.nvim",
-   run = function()
-    vim.fn["mkdp#util#install"]()
-   end,
-  })
+  "nvim-treesitter/nvim-treesitter",
+  run = function()
+   require("nvim-treesitter.install").update({ with_sync = true })
+   require 'nvim-treesitter.install'.compilers = { "gcc" }
+  end,
+ })
+  use("nvim-treesitter/playground")
 
 end)
