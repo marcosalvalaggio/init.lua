49b71de17393a9ec25a080ceeda7b8db3b1f32fd
mar.salvalaggio@gmail.com <55287795+Salva32@users.noreply.github.com>
2023-09-11 22:22:19 +0200

    wip

--- after/plugin/foio.lua
+++ after/plugin/foio.lua
@@ -1,3 +1,4 @@
+
 function ToggleTaskPriority(priority)
     local line = vim.fn.getline('.')
     local new_line
@@ -13,8 +14,31 @@ function ToggleTaskPriority(priority)
     vim.fn.setline('.', new_line)
 end
 
+function ToggleTaskStatus(input)
+    local line = vim.fn.getline('.')
+    local new_line
+
+    if input == 7 then
+        new_line = string.gsub(line, '%(%s*[%a%s,]*%s*%)', '')
+    elseif input == 4 then
+        new_line = string.gsub(line, '%(%s*[%a%s,]*%s*%)', '')
+        new_line = new_line .. "(todo)"
+    elseif input == 5 then
+        new_line = string.gsub(line, '%(%s*[%a%s,]*%s*%)', '')
+        new_line = new_line .. "(wip)"
+    elseif input == 6 then
+        new_line = string.gsub(line, '%(%s*[%a%s,]*%s*%)', '')
+        new_line = new_line .. "(done)"
+    end
+
+    vim.fn.setline('.', new_line)
+end
+
 vim.api.nvim_set_keymap('n', '<leader>1', [[:lua ToggleTaskPriority(1)<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>2', [[:lua ToggleTaskPriority(2)<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>3', [[:lua ToggleTaskPriority(3)<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>0', [[:lua ToggleTaskPriority(0)<CR>]], { noremap = true, silent = true })
-
+vim.api.nvim_set_keymap('n', '<leader>7', [[:lua ToggleTaskStatus(7)<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>4', [[:lua ToggleTaskStatus(4)<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>5', [[:lua ToggleTaskStatus(5)<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>6', [[:lua ToggleTaskStatus(6)<CR>]], { noremap = true, silent = true })
