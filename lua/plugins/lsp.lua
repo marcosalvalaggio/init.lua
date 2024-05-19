5fb1d9085c7ab7d07174f3d7806323ed919d7c3b
marcosalvalaggio <mar.salvalaggio@gmail.com>
2024-05-19 11:49:58 +0200

    wip lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,55 +1,49 @@
 return {
-    -- {
-    --     "williamboman/mason.nvim",
-    --     lazy = false,
-    --     config = function()
-    --         require("mason").setup()
-    --     end,
-    -- },
-    -- {
-    --     "williamboman/mason-lspconfig.nvim",
-    --     lazy = false,
-    --     opts = {
-    --         auto_install = true,
-    --     },
-    --     config = function()
-    --         require("mason-lspconfig").setup({
-    --             ensure_installed = {
-    --                 "lua_ls",
-    --                 "pyright",
-    --                 "clangd",
-    --                 "html",
-    --                 "quick_lint_js",
-    --                 "tsserver"
-    --             }
-    --         })
-    --     end
-    -- },
+    {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
     {
-        "neovim/nvim-lspconfig",
+        'neovim/nvim-lspconfig',
         lazy = false,
         config = function()
-            local capabilities = require('cmp_nvim_lsp').default_capabilities()
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities
-            })
-            lspconfig.pyright.setup({
-                 capabilities = capabilities
-            })
-            -- lspconfig.quick_lint_js.setup{}
-            lspconfig.tsserver.setup({
-                 capabilities = capabilities
-            })
-            lspconfig.rust_analyzer.setup({
-                 capabilities = capabilities
-            })
+            local lspconfig = require('lspconfig')
+            local cmp_nvim_lsp = require('cmp_nvim_lsp')
+            local capabilities = cmp_nvim_lsp.default_capabilities()
+            lspconfig.lua_ls.setup{capabilities = capabilities}
+            lspconfig.pyright.setup{capabilities = capabilities}
+            lspconfig.tsserver.setup{capabilities = capabilities}
+            lspconfig.rust_analyzer.setup{capabilities = capabilities}
             -- keymap
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-            vim.keymap.set({'n','v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+            vim.keymap.set('n', 'go', vim.lsp.buf.type_definition, {})
+            vim.keymap.set('n', 'gf', vim.lsp.buf.rename, {})
+          vim.keymap.set({'n','v'}, '<leader>ca', vim.lsp.buf.code_action, {})
         end
-    }
 
+    },
+    {
+        'hrsh7th/cmp-nvim-lsp',
+        config = function()
+            local cmp = require('cmp')
+            local luasnip = require('luasnip')
+            cmp.setup({
+                snippet =  {
+                    expand = function(args)
+                        luasnip.lsp_expand(args.body)
+                    end,
+                },
+                mapping = {
+                    ['<C-y>'] = cmp.mapping.confirm({select = false}),
+                    ['<C-e>'] = cmp.mapping.abort(),
+                    ['<Up>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
+                    ['<Down>'] = cmp.mapping.select_next_item({behavior = 'select'}),
+                },
+                sources = {
+                    {name='nvim_lsp'},
+                    {name='luasnip'}
+                }
+            })
+        end
+    },
+    {'hrsh7th/nvim-cmp'},
+    {'L3MON4D3/LuaSnip'},
 }
-
