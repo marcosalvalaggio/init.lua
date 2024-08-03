7729acd1c1d5eba014e3a6847e66abec9501324d
marcosalvalaggio <mar.salvalaggio@gmail.com>
2024-08-03 16:44:53 +0200

    wip

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,6 +5,8 @@ vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.swapfile = false
+vim.opt.guicursor = "n-v-i-c:block-Cursor"
+vim.opt.colorcolumn = "120"
 
 -- Remap
 vim.g.mapleader = " "
