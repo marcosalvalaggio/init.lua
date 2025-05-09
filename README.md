2cf0e06a0b25fc9f8cf66dc8cb60847a1cc4ba2d
marcosalvalaggio <mar.salvalaggio@gmail.com>
2025-05-10 00:33:41 +0200

    wip

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 * [Neovim](https://neovim.io/) 
 * [Lazy](https://github.com/folke/lazy.nvim) 
 * [Git](https://git-scm.com/)
+* [ComicShannsMono Nerd Font](https://www.nerdfonts.com/font-downloads) 
--- lua/plugins/funktree.lua
+++ lua/plugins/funktree.lua
@@ -1,5 +1,5 @@
 return {
-	"marcosalvalaggio/funktree",
+    "marcosalvalaggio/funktree",
     dependencies = {"nvim-telescope/telescope.nvim"},
 	config = function()
 		vim.api.nvim_set_keymap("n", "<leader>k", ":Funk<CR>", { noremap = true })
